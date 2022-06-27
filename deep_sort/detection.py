9f8af9aee94415a07fbcdd40d24fe8a6aa46db6b
spierballenbassieman <b.heuver@student.ru.nl>
2022-06-27 19:21:31 +0200

    Add color attribute to detection

--- deep_sort/detection.py
+++ deep_sort/detection.py
@@ -28,14 +28,18 @@ class Detection(object):
 
     """
 
-    def __init__(self, tlwh, confidence, class_name, feature):
+    def __init__(self, tlwh, confidence, class_name, feature, color=None):
         self.tlwh = np.asarray(tlwh, dtype=np.float)
         self.confidence = float(confidence)
         self.class_name = class_name
         self.feature = np.asarray(feature, dtype=np.float32)
+        self.colore = color
 
     def get_class(self):
         return self.class_name
+    
+    def get_color(self):
+        return self.colorx
 
     def to_tlbr(self):
         """Convert bounding box to format `(min x, min y, max x, max y)`, i.e.,

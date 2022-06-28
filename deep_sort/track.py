0cecab73b825e5ce44d78bfd63f4442a5d5fce69
spierballenbassieman <b.heuver@student.ru.nl>
2022-06-28 02:30:05 +0200

    Update track.py

--- deep_sort/track.py
+++ deep_sort/track.py
@@ -131,6 +131,8 @@ class Track:
     def set_last(self):
         self.last_mean = self.mean
         self.last_covariance = self.covariance
+        if len(self.colors) > 0:
+            self.last_color = self.colors[-1]
         
     
     def predict(self, kf):
@@ -172,20 +174,23 @@ class Track:
         ### this part used to be just the features.append(detection.feature)
         if self.get_color() == detection.color:
             self.features.append(detection.feature)
+            
+            if detection.color is not None:
+                if len(self.colors) < 300:
+                    self.colors.append(detection.color)
+                else:
+                    del self.colors[0]
+                    self.colors.append(detection.color)
+                
         elif self.get_color() != detection.color:
             self.mean = self.last_mean
             self.covariance = self.last_covariance
             self.features.append(detection.feature)
-            if len(self.colors) > 0:
-                self.colors.pop()
+#             if len(self.colors) > 0:
+#                 self.colors.pop()
         ###
         
-        if detection.color is not None:
-            if len(self.colors) < 300:
-                self.colors.append(detection.color)
-            else:
-                del self.colors[0]
-                self.colors.append(detection.color)
+        
                 
         self.hits += 1
         self.time_since_update = 0
